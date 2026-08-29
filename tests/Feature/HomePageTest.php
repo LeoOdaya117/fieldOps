3f53c74e4eb16cd64a12aba891fd8a7ab4b09038
LeoOdaya117 <odayaleo117@gmail.com>
2026-08-29 13:23:59 +0800

    test: prepare homepage for visit logging

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Inertia\Testing\AssertableInertia as Assert;
 use Tests\TestCase;
 
 class HomePageTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_the_public_home_page_renders_the_welcome_inertia_page(): void
     {
         $this->get(route('home'))
